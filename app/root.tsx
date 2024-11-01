776bb360397876dba379466a088f69a2ba5c4189
noViceMin <admin@isamin.kr>
2024-11-01 15:19:50 +0900

    feat: google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,17 @@ const App = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#f57b51" />
         <meta name="keywords" content="노비스민,novicemin,이즈어민,isamin,박성민,parkseongmin,seongminpark,포트폴리오" />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-631HMKP7NM" />
+        <script async id="gtag-init" dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-631HMKP7NM');
+          `
+        }} />
         <Meta />
         <Links />
       </head>
